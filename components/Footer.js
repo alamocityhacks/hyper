3e0e49d870f981a6963135c4a68793009bf9554c
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-01-10 14:48:14 -0600

    🐛 add keys to footer list

--- components/Footer.js
+++ components/Footer.js
@@ -10,13 +10,15 @@ const links = [
 export default function Footer() {
     return (
         <div id="footer" className="mt-32 text-black font-inter font-semibold uppercase text-center">
-            <div id="footernav" className="mb-10 flex flex-col md:flex-row space-y-6 md:space-x-12 md:space-y-0 md:w-screen text-xl justify-center items-center">
-                {links.map(({name, href}) => (
-                    <Link href={href ?? ``}>
-                        <a target="_blank" className="hover:bg-black hover:bg-opacity-5 px-4 py-2 rounded-xl">{name ?? ``}</a>
-                    </Link>
+            <ul id="footernav" className="mb-10 flex flex-col md:flex-row space-y-6 md:space-x-12 md:space-y-0 md:w-screen text-xl justify-center items-center">
+                {links.map(({ name, href }) => (
+                    <li key={name}>
+                        <Link href={href ?? ``}>
+                            <a target="_blank" className="hover:bg-black hover:bg-opacity-5 px-4 py-2 rounded-xl">{name ?? ``}</a>
+                        </Link>
+                    </li>
                 ))}
-            </div>
+            </ul>
             <h1 className="opacity-60 px-4 md:px-0 text-2xl md:text-3xl mb-4">copyright alamo city hacks {new Date().getFullYear()} all rights reserved</h1>
         </div>
     )
