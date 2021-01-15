aa8ee25e2d563c1d4da74bb85248946ac1478970
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-01-14 23:40:41 -0600

    🥅 Improve error handling on pages

--- components/ChallengesPage.js
+++ components/ChallengesPage.js
@@ -17,24 +17,40 @@ export default function ChallengesPage() {
                         <h1 className="text-4xl font-bold">Quests</h1>
                         <div className="mt-4 flex flex-col space-y-4">
                             {
-                                user.userInfo.roles["SS"].includes("Beginner") ?
+                                user.userInfo?.roles["SS"].includes("Beginner") ?
                                     challenges[0].map(({number, name, points, difficulty}) => (
-                                        <div>{name}</div>
+                                        <Link href={user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? '' : `/quests/${number}`}>
+                                            <div className={`${user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? 'bg-green bg-opacity-80' : 'border'} p-5 rounded-xl hover:border-transparent ${user.userInfo.completedChallenges["NS"].includes(number.toString()) ? '' : 'hover:bg-black hover:bg-opacity-10'}`}>
+                                                <div className="flex flex-row space-x-2">
+                                                    {number}
+                                                    <h2 className="ml-2 inline font-bold text-3xl">{name}</h2>
+                                                </div>
+                                                <p>Points: {points} Difficulty: {difficulty}</p>
+                                            </div>
+                                        </Link>
                                     ))
                                 : ''
                             }
                             {
-                                user.userInfo.roles["SS"].includes("Intermediate") ?
+                                user.userInfo?.roles["SS"].includes("Intermediate") ?
                                     challenges[1].map(({number, name, points, difficulty}) => (
-                                        <div>{name}</div>
+                                        <Link href={user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? '' : `/quests/${number}`}>
+                                            <div className={`${user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? 'bg-green bg-opacity-80' : 'border'} p-5 rounded-xl hover:border-transparent ${user.userInfo.completedChallenges["NS"].includes(number.toString()) ? '' : 'hover:bg-black hover:bg-opacity-10'}`}>
+                                                <div className="flex flex-row space-x-2">
+                                                    {number}
+                                                    <h2 className="ml-2 inline font-bold text-3xl">{name}</h2>
+                                                </div>
+                                                <p>Points: {points} Difficulty: {difficulty}</p>
+                                            </div>
+                                        </Link>
                                     ))
                                 : ''
                             }
                             {
-                                user.userInfo.roles["SS"].includes("Advanced") ?
+                                user.userInfo?.roles["SS"].includes("Advanced") ?
                                     challenges[2].map(({number, name, points, difficulty}) => (
-                                        <Link href={user.userInfo.completedChallenges["NS"].includes(number.toString()) ? '' : `/quests/${number}`}>
-                                            <div className={`${user.userInfo.completedChallenges["NS"].includes(number.toString()) ? 'bg-green bg-opacity-80' : 'border'} p-5 rounded-xl hover:border-transparent ${user.userInfo.completedChallenges["NS"].includes(number.toString()) ? '' : 'hover:bg-black hover:bg-opacity-10'}`}>
+                                        <Link href={user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? '' : `/quests/${number}`}>
+                                            <div className={`${user.userInfo?.completedChallenges["NS"].includes(number.toString()) ? 'bg-green bg-opacity-80' : 'border'} p-5 rounded-xl hover:border-transparent ${user.userInfo.completedChallenges["NS"].includes(number.toString()) ? '' : 'hover:bg-black hover:bg-opacity-10'}`}>
                                                 <div className="flex flex-row space-x-2">
                                                     {number}
                                                     <h2 className="ml-2 inline font-bold text-3xl">{name}</h2>
--- pages/profile.js
+++ pages/profile.js
@@ -14,9 +14,9 @@ const Profile = () => {
           <Nav />
           <div className="font-inter max-w-xl mx-6 md:mx-auto mt-16 bg-black bg-opacity-5 rounded-xl relative z-30 p-8 flex flex-col space-y-4">
             <h1 className="font-bold text-4xl">Group Info</h1>
-            { user.userInfo.points ? <div className="text-center py-5 border-2 rounded-xl md:w-4/12">
+            { user.userInfo?.points ? <div className="text-center py-5 border-2 rounded-xl md:w-4/12">
               <div className="font-rubik text-7xl font-black">
-                {user.userInfo.points["N"]}
+                {user.userInfo?.points["N"]}
               </div>
               <h3 className="font-rubik uppercase text-4xl font-bold">Points</h3>
             </div> : '' }
@@ -24,37 +24,37 @@ const Profile = () => {
               <h3 className="font-bold">Ethereum Blockchain Issuer ID</h3>
               <div className="break-words"><code>{user.issuer}</code></div>
             </div>
-            { user.userInfo.admin ? <div>
+            { user.userInfo?.admin ? <div>
               <h3 className="font-bold">Group Name</h3>
-              {user.userInfo.admin["BOOL"] ? 'Admin' : user.userInfo.name["S"]}
+              {user.userInfo?.admin["BOOL"] ? 'Admin' : user.userInfo?.name["S"]}
             </div> : '' }
             <div>
               <h3 className="font-bold">Email</h3>
               {user.email}
             </div>
-            { user.userInfo.completedChallenges && user.userInfo.challenges ? <div>
+            { user.userInfo?.completedChallenges && user.userInfo?.challenges ? <div>
               <h3 className="font-bold">Completed Challenges</h3>
               <div className="mt-2 flex flex-wrap">
-                {Array.from({ length: user.userInfo.challenges["N"] }, (_, i) => i + 1).sort(((a, b) => a - b)).map((number) => (
-                  <div className={`mr-2 mb-2 border-transparent inline-block px-3 font-bold font-3xl ${user.userInfo.completedChallenges["NS"].map(Number).includes(number) ? 'bg-green bg-opacity-20' : 'bg-black bg-opacity-10'} rounded-lg`}>
+                {Array.from({ length: user.userInfo?.challenges["N"] }, (_, i) => i + 1).sort(((a, b) => a - b)).map((number) => (
+                  <div className={`mr-2 mb-2 border-transparent inline-block px-3 font-bold font-3xl ${user.userInfo?.completedChallenges["NS"].map(Number).includes(number) ? 'bg-green bg-opacity-20' : 'bg-black bg-opacity-10'} rounded-lg`}>
                     {number}
                   </div>
                 ))}
               </div>
             </div> : '' }
-            { user.userInfo.groupMembers ? <div>
+            { user.userInfo?.groupMembers ? <div>
               <h3 className="font-bold">Group Members</h3>
               <div className="mt-2 flex flex-wrap">
-                {user.userInfo.groupMembers["SS"].sort().map((name) => (
+                {user.userInfo?.groupMembers["SS"].sort().map((name) => (
                     <div className="mr-2 mb-2 border-transparent inline-block px-3 font-bold font-3xl bg-black bg-opacity-10 rounded-lg" style={{ backgroundColor: '#' + toHex(name).substring(9, 13) + 'FF', color: 'white' }}>
                       {name}
                     </div>
                 ))}
               </div>
             </div> : '' }
-            { user.userInfo.roles ? <div>
+            { user.userInfo?.roles ? <div>
               <h3 className="font-bold">Roles</h3>
-              { user.userInfo.roles["SS"].map(role => (
+              { user.userInfo?.roles["SS"].map(role => (
                 <div className="capitalize mr-2 my-2 border-transparent inline-block px-3 font-bold font-3xl bg-black bg-opacity-10 rounded-lg">
                   {role}
                 </div>
